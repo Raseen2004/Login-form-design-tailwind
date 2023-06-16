c2d665a42bdb98238f1275fe18ac90c7c48ffaf7
Raseen2004 <rajaraseen105@gmail.com>
2023-06-16 23:00:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+      // colors: {
+        // transparent: 'transparent',
+        // current: 'currentColor',
+        // 'purple': '#6554AF',
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
